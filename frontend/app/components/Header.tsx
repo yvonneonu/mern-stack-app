5725536ff1f2d77f184e5fa23e57dd4ef07f7e40
yvonneonu <yvonneonuorah@gmail.com>
2024-03-29 18:48:54 +0100

    git update

---
+++ frontend/app/components/Header.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Link from "next/link";
+import { useAppContext } from "../contexts/AppContext";
+import SignOutButton from "./SignOutButton";
+
+const Header = () => {
+  const { isLoggedIn } = useAppContext();
+
+  return (
+    <div className="bg-blue-800 py-6">
+      <div className="container mx-auto flex justify-between">
+        <span className="text-3xl text-white font-bold tracking-tight">
+          <Link href="/">MernHolidays.com</Link>
+        </span>
+        <span className="flex space-x-2">
+          {isLoggedIn ? (
+            <>
+              <Link
+                className="flex items-center text-white px-3 font-bold hover:bg-blue-600"
+                href="/my-bookings"
+              >
+                My Bookings
+              </Link>
+              <Link
+                className="flex items-center text-white px-3 font-bold hover:bg-blue-600"
+                href="/my-hotels"
+              >
+                My Hotels
+              </Link>
+              <SignOutButton />
+            </>
+          ) : (
+            <Link
+              href="/sign-in"
+              className="flex bg-white items-center text-blue-600 px-3 font-bold hover:bg-gray-100"
+            >
+              Sign In
+            </Link>
+          )}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ frontend/app/services/api-client.ts
@@ -0,0 +1,57 @@
+import { RegisterFormData } from "../register/page";
+import { SignInFormData } from "../sign-in/page";
+
+const API_BASE_URL = process.env.NEXT_APP_API_BASE_URL || "";
+export const register = async (formData: RegisterFormData) => {
+  const response = await fetch(`${API_BASE_URL}/api/users/register`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
+  const responseBody = await response.json();
+  if (!response.ok) {
+    throw new Error(responseBody.message);
+  }
+};
+// export function validateToken(arg0: string, validateToken: any, arg2: { retry: false; }): { isError: any; } {
+//     throw new Error("Function not implemented.");
+// }
+
+export const signIn = async (formData: SignInFormData) => {
+  const response = await fetch(`${API_BASE_URL}/api/auth/login`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
+  const body = await response.json();
+  if (!response.ok) {
+    throw new Error(body.message);
+  }
+  return body;
+};
+
+export const validateToken = async () => {
+  const response = await fetch(`${API_BASE_URL}/api/auth/validate-token`, {
+    credentials: "include",
+  });
+  if (!response.ok) {
+    throw new Error("Invalid token");
+  }
+  return response.json();
+};
+export const signOut = async () => {
+  const response = await fetch(`${API_BASE_URL}/api/auth/logout`, {
+    credentials: "include",
+    method: "POST",
+  });
+  if (!response.ok) {
+    throw new Error("Error during sign out");
+  }
+  return response.json();
+};
